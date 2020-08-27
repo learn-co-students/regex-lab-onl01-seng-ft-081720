8710990ddd872c2e795ce5af2ad99837b4c917a1
ts-miller <tyler@DESKTOP-RNOETJO.localdomain>
2020-08-27 17:41:24 -0400

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,20 @@
 def starts_with_a_vowel?(word)
-
+    !!(word =~ /\b[aeiou]/i)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/\bun\w*/)
+    #text.scan(/\bun\w*/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    !!(text =~ /^[A-Z]/) && !!(text =~ /\.$/)
 end
 
 def valid_phone_number?(phone)
-
+    !!(phone =~ /\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}/)
 end
